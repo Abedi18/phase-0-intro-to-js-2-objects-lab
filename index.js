2af7c396fe2d626af309527eb8a988e65eabae91
Abednego <abedikroos15@gmail.com>
2023-03-21 12:31:47 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// Write your solution in this file!
+let employee = {
+    name: "Sam",
+    streetAddress: "11 Broadway"
+  };
+  
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return { ...employee, [key]: value };
+  }
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+  
+  console.log(employee);
+  
+  // updateEmployeeWithKeyAndValue
+  const updatedEmployee = updateEmployeeWithKeyAndValue(employee, "streetAddress", "456 Main St");
+  console.log(updatedEmployee); 
+  console.log(employee); 
+  
+  // destructivelyUpdateEmployeeWithKeyAndValue
+  const destructivelyUpdatedEmployee = destructivelyUpdateEmployeeWithKeyAndValue(employee, "streetAddress", "789 Main St");
+  console.log(destructivelyUpdatedEmployee); 
+  console.log(employee); 
+  
+  // deleteFromEmployeeByKey
+  const deletedEmployee = deleteFromEmployeeByKey(employee, "streetAddress");
+  console.log(deletedEmployee);
+  console.log(employee); 
+  
+  // destructivelyDeleteFromEmployeeByKey
+  const destructivelyDeletedEmployee = destructivelyDeleteFromEmployeeByKey(employee, "streetAddress");
+  console.log(destructivelyDeletedEmployee); 
+  console.log(employee); 
+  
+  
+  
+  
